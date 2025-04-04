9176c1c90aae4489b62ada046c68de2262c57424
IhantsaFana <ihantsarakotondranaivo@gmail.com>
2025-04-04 21:59:54 +0300

    Nandefa anle fatinjavatra kely tsy aiko reo

---
+++ entrepreneur/assets/tilset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tilset" tilewidth="32" tileheight="32" tilecount="72" columns="8">
+ <image source="image/map/tileset_16x16_final_1.png" width="272" height="304"/>
+</tileset>
